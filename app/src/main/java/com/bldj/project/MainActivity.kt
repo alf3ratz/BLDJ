e3e26c249de502e725a63cd346b071a2fba49599
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-04-22 17:52:53 +0300

    шото накалякал

--- app/src/main/java/com/bldj/project/MainActivity.kt
+++ app/src/main/java/com/bldj/project/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.bldj.project
+package course.ru.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import course.ru.myapplication.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
     }
 }
